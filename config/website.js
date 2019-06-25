24bfc8f20cb72c26bc7571bf17be1a7225563d98
Nishant Kumar <nihantanu@gmail.com>
2019-06-25 10:28:27 +0200

    Initial Commits

---
+++ config/website.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Nishant Kr.', // Navigation and Site Title
+  siteTitleAlt: 'The personal website of Nishant K.', // Alternative Site title for SEO
+  siteTitleShort: 'nishantkr', // short_name for manifest
+  siteUrl: 'https://nishantkr.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription:
+    'Come check out how Nishant Kr. can help you level up your career as a software engineer.',
+  author: 'Nishant Kr.', // Author for schemaORGJSONLD
+  organization: 'Nishant Kr.',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@mfssharma', // Twitter Username
+  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/mfssharma/',
+  twitterHandle: '@mfssharma',
+  github: 'https://github.com/nishant8bits/',
+  linkedin: 'https://www.linkedin.com/in/nishantkumar1/',
+}
